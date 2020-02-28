4a0ca9210d70c5e7bda10f67b5b0efcefc70d6ae
Quang Phu <quangphu1799@gmail.com>
2020-02-28 21:52:19 +0700

    update

--- Grammar/convert_html.py
+++ Grammar/convert_html.py
@@ -7,15 +7,15 @@ import shutil
 import time
 from threading import Thread
 from shutil import copyfile
-import requests
+# import requests
 
 TITLE_COLOR = '#37003C'
 TEXT_COLOR = '#333300'
 RED_COLOR = '#FF0000'
-TITLE_FONT_SIZE = '30px'
-SECTION_FONT_SIZE = '24px'
-TEXT_FONT_SIZE = '18px'
-HL_FONT_SIZE = '20px'
+TITLE_FONT_SIZE = '28px'
+SECTION_FONT_SIZE = '22px'
+TEXT_FONT_SIZE = '16px'
+HL_FONT_SIZE = '16px'
 BOLD_BEGIN = '<strong>'
 BOLD_END = '</strong>'
 HIGHLIGHT_BEGIN = '<u>'
@@ -30,10 +30,17 @@ def check_character(character, line):
     else:
         return -1
 
-def open_file():
-    fopen = open(sys.argv[1] + '.txt')
-    fopen_json = open(sys.argv[1] + '.html', "w+")
+def open_file(file):
+    fopen = open(file + '.txt')
+    fopen_json = open(file + '.html', "w+")
     fopen_json.truncate()
+    fopen_json.write('<!DOCTYPE html>\n' + '<html>\n' + '<body>\n')
+    fopen_json.write('<head>\n')
+    fopen_json.write('<meta charset="UTF-8">\n')
+    # fopen_json.write('<style>\n')
+    # fopen_json.write('padding: 35px;\n')
+    # fopen_json.write('</style>')
+    fopen_json.write('</head>\n')
     for line in fopen:
         line = line.rstrip()
         if line.startswith('~'):
@@ -48,47 +55,47 @@ def open_file():
                             + BOLD_BEGIN + HIGHLIGHT_BEGIN + line[1:] \
                             + HIGHLIGHT_END + BOLD_END + '</span>')
         elif line.startswith('!'):
-            fopen_json.write('<br>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
+            fopen_json.write('<p>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
                             + '; color: ' + TITLE_COLOR + ';">' \
                             + BOLD_BEGIN + HIGHLIGHT_BEGIN + line[1:] \
-                            + HIGHLIGHT_END + BOLD_END + '</span>')
+                            + HIGHLIGHT_END + BOLD_END + '</span></p>\n')
         elif line.startswith('@'):
-            fopen_json.write('<br>' + '<span style="font-size: ' + HL_FONT_SIZE \
-                            + '; color: ' + RED_COLOR + ';">' \
-                            + '&nbsp;&nbsp;&nbsp;&nbsp;' + BOLD_BEGIN + line[1:] + BOLD_END + '</span>')
+            fopen_json.write('<p>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
+                                + '; color: ' + TEXT_COLOR + ';">' + BOLD_BEGIN + line[1:] + BOLD_END + '</span></p>\n')
         elif line.startswith('- '):
             check = check_character(':', line)
             if check == -1:
-                fopen_json.write('<br>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
-                                + '; color: ' + TEXT_COLOR + ';">' + BOLD_BEGIN + '>&#9989;' + line[1:] + BOLD_END + '</span>')
+                fopen_json.write('<p>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
+                                + '; color: ' + TEXT_COLOR + ';">' + BOLD_BEGIN + '-' + line[1:] + BOLD_END + '</span></p>\n')
             else:
-                fopen_json.write('<br>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
-                                + '; color: ' + TEXT_COLOR + ';">' + BOLD_BEGIN + '>&#9989;' + line[1:check] + BOLD_END + line[check:] + '</span>')
+                fopen_json.write('<p>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
+                                + '; color: ' + TEXT_COLOR + ';">' + BOLD_BEGIN + '-' + line[1:check] + BOLD_END + line[check:] + '</span></p>\n')
 
         elif line.startswith(' + '):
             check = check_character(':', line)
             if check == -1:
-                fopen_json.write('<br>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
+                fopen_json.write('<p>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
                                 + '; color: ' + TEXT_COLOR + ';">' + '&nbsp;&nbsp;' \
-                                + BOLD_BEGIN + '&#10146;' + line[2:] + BOLD_END + '</span>')
+                                + BOLD_BEGIN + '&bull;' + line[2:] + BOLD_END + '</span></p>\n')
             else:
-                fopen_json.write('<br>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
+                fopen_json.write('<p>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
                                 + '; color: ' + TEXT_COLOR + ';">' + '&nbsp;&nbsp;' \
-                                + BOLD_BEGIN + '&#10146;' + line[2:check] + BOLD_END + line[check:] + '</span>')
+                                + BOLD_BEGIN + '&bull;' + line[2:check] + BOLD_END + line[check:] + '</span></p>\n')
 
         elif line.startswith('   '):
-            fopen_json.write('<br>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
+            fopen_json.write('<p>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
                             + '; color: ' + TEXT_COLOR + ';">' \
-                            + ITALIC_BEGIN + '&nbsp;&nbsp;&nbsp;&nbsp;' + '&#9998;' + line + ITALIC_END + '</span>')
+                            + ITALIC_BEGIN + '&nbsp;&nbsp;&nbsp;&nbsp;' + '&rArr;' + line + ITALIC_END + '</span></p>\n')
         elif line.startswith('  '):
-            fopen_json.write('<br>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
+            fopen_json.write('<p>' + '<span style="font-size: ' + TEXT_FONT_SIZE \
                             + '; color: ' + TEXT_COLOR + ';">' \
-                            + ITALIC_BEGIN + '&nbsp;&nbsp;&nbsp;' + '&#9998;'  + line + ITALIC_END + '</span>')
+                            + ITALIC_BEGIN + '&nbsp;&nbsp;&nbsp;' + '&rArr;'  + line + ITALIC_END + '</span></p>\n')
 
-        elif line.startswith('---'):
-            fopen_json.write('</p>' + "\n")
+        # elif line.startswith('---'):
+        #     fopen_json.write('</p>' + "\n")
         else:
-            print('Missing line' + line)
+            print('Missing line' + line + " " + file)
+    fopen_json.write('</body>\n' + '</html>\n')
     fopen_json.close()
     fopen.close()
 
@@ -99,7 +106,14 @@ def convert_file():
 
 def main():
     # create json file
-    open_file()
+    for filename in os.listdir(os.getcwd()):
+        if filename.endswith(".txt"):
+            # print(os.path.splitext(filename)[0])
+            open_file(os.path.splitext(filename)[0])
+         # print(os.path.join(directory, filename))
+            continue
+        else:
+            continue
     print(check_character('i', 'abcdefgh'))
 
 if __name__ == '__main__':
